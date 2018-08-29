caf310b76c2189de24f0dc1aabf51f99bc8e6103
Asavari <tayal1asavari@gmail.com>
2018-08-29 00:21:47 -0700

    login changes

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@
 set -e -x
 
 az login --service-principal --username $APP_ID --password $PASSWORD --tenant $TENANT_ID
-func azure account login -u $ACCUSERNAME -p $ACCPASSWORD
+func azure login -u $ACCUSERNAME -w $ACCPASSWORD
 func azure functionapp publish astaytestapp16 --build-native-deps --force
